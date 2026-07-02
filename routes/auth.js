cf6e5bb1c75e03d33009ccaf93abd5d6ff0c19fa
Rikisha-mahrajan <rikisham552@gmail.com>
2026-07-02 16:35:00 +0545

    fix smtp port to 587

--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,9 @@ const nodemailer = require('nodemailer');
 const db = require('../config/db');
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
